322d327fe55ec0485daf5aefb62b7f2756f0a129
Rafael Petit <rpetit.dev@gmail.com>
2025-03-12 11:25:59 -0300

    feat: construtor classer server

--- src/main/java/com/example/berkeleysockets/Server.java
+++ src/main/java/com/example/berkeleysockets/Server.java
@@ -1,5 +1,29 @@
 package com.example.berkeleysockets;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+
+    private ServerSocket serverSocket;
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+
+    public Server(ServerSocket serverSocket) {
+        try {
+            this.serverSocket = serverSocket;
+            this.socket = serverSocket.accept();
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        } catch (IOException e) {
+            System.out.println("Erro ao criar o servidor");
+            e.printStackTrace();
+        }
+    }
+
+
+
 
 }
